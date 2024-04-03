ea05508881abb048ff072330b4d25495fdfe67eb
Safwan Pulisseri <safwanpulisseri123@gmail.com>
2024-04-03 20:40:11 +0530

    almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:counter_app_with_provider/counter%20notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,15 +11,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(
-        title: 'Provider',
+    return MultiProvider(
+      providers: [
+        //deffrent types providers(proxy,future)
+        ChangeNotifierProvider<CounterNotifier>(
+          create: (_) => CounterNotifier(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+          useMaterial3: true,
+        ),
+        home: const MyHomePage(
+          title: 'Provider',
+        ),
       ),
     );
   }
@@ -33,17 +43,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    print('runnung My App');
+    final counterData = Provider.of<CounterNotifier>(context, listen: false);
+    print('running My App');
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -56,15 +59,17 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Consumer<CounterNotifier>(builder: (context, data, _) {
+              return Text(
+                data.counter.toString(),
+                style: Theme.of(context).textTheme.headlineMedium,
+              );
+            }),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: counterData.increment,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
